da4e6853b20fe21cd69341e412c7d3e7759351a8
LilyBurton <lil@192.168.1.108>
2024-04-30 13:43:39 +0100

    RSpec testing, times two numbers together, if either number is less than 0, return 0

---
+++ lib/paperwork.rb
@@ -0,0 +1,9 @@
+class Paperwork
+    def self.paperwork(n, m)
+        if n < 0 || m < m
+            return 0
+        else
+            n * m
+        end
+    end
+end
---
+++ spec/paperwork_spec.rb
@@ -0,0 +1,17 @@
+require "paperwork"
+
+describe ".paperwork" do
+    context "times two positive numbers" do
+        it "returns the answer" do
+            expect(Paperwork.paperwork(5, 5)).to eql(25)
+        end
+    end
+end
+
+describe ".paperwork" do
+    context "if n or m is less than 0" do
+        it "returns 0" do
+            expect(Paperwork.paperwork(5, 0)).to eql(0)
+        end
+    end
+end
